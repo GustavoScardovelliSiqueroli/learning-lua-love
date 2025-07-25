e060f12d0398b94c2d7c79242de69f8c606a6e90
gustavo <gustavosecardovelli@gmail.com>
2025-07-25 16:33:23 -0300

    att

--- main.lua
+++ main.lua
@@ -1,18 +1,22 @@
 NUM = 0
 IMAGE = nil
+IMG_SCALE = 2.5
 
-local window_width, window_height, _ = love.window.getMode()
-local imgx, imgy = 0, 0
+local window_width, window_height = love.window.getMode()
 
 function love.draw()
-	love.graphics.draw(IMAGE, imgx, imgy, 0, 0.5, 3)
+	local imgx, imgy = calcule_image_center_points(IMAGE)
+	love.graphics.draw(IMAGE, imgx, imgy, 0, IMG_SCALE, IMG_SCALE)
 	love.graphics.print("Olá Mundo!", 350, 250)
-	love.graphics.print(window_width, 350, 350)
+	love.graphics.print(NUM, 350, 350)
+	love.graphics.print(love.timer.getFPS(), 0, 0)
+	love.graphics.print(imgx .. " " .. imgy, 350, 370)
+	love.graphics.print("image width and height: " .. IMAGE:getWidth() .. " " .. IMAGE:getHeight(), 350, 390)
+	love.graphics.print("window: " .. window_width .. " " .. window_height, 350, 410)
 end
 
 function love.load()
 	IMAGE = love.graphics.newImage("cake.jpg")
-	-- imgx, imgy = -IMAGE:getWidth() / 2, -IMAGE:getHeight() / 2
 
 	local f = love.graphics.newFont(20)
 	love.graphics.setFont(f)
@@ -24,9 +28,15 @@ function love.update(dt)
 	end
 end
 
-function love.mousepressed(x, y, button, istouch)
+function love.mousepressed(_, _, button)
 	if button == 1 then
-		imgx = x -- move a imagem para onde o mouse foi clicado
-		imgy = y
+		if IMG_SCALE < 4 and IMG_SCALE >= 0.5 then
+			IMG_SCALE = IMG_SCALE - 0.1
+		end
 	end
+end
+
+function calcule_image_center_points(img)
+	local ix, iy = img:getWidth() * IMG_SCALE, img:getHeight() * IMG_SCALE
+	return ((window_width - ix) / 2), ((window_height - iy) / 2)
 end
